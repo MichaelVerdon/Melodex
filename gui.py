2f697d134a053fed181428d3ff422d0a639c0a04
Alvin-Fung <alvin_fung9@outlook.com>
2023-06-05 18:32:22 +0100

    Rewrote the Window class for now
    Can't figure out at the moment on how to effectively use classes for attributing the app.

--- gui.py
+++ gui.py
@@ -2,31 +2,24 @@ import tkinter as tk
 from tkinter import *
 
 #class for making a window
-class Window(tk.Tk):
+class Window():
     def __init__(self):
         super().__init__()
-        self.title("Melodex")
-        self.attributes= {
-            "bg": 'white',
-            "width":'1024',
-            "height": '768'  
-        }
-        self.maxsize = (1024,768)
-        self.minsize = (1024, 768)
-        self.resizable(0,0)
-        self.configure(self.attributes)
+        self.root = tk.Tk()
+        self.root.geometry('1280x720')
+        self.root.title("Melodex")
+        self.mainframe = tk.Frame(self.root ,background='white')
+        self.mainframe.pack(fill='both', expand= True)
         
-        self.text = tk.Label(self, text ='Melodex', background='white', font=("Roboto Mono", 24))
+        self.text = tk.Label(self.mainframe, text ='Melodex', background='white', font=("Roboto Mono", 24))
         self.text.grid(row=0, column=0)
+        self.root.mainloop()
+        return
 
 class App(tk.Frame):
     def __init__(self, parent):
         super().__init__(parent)
-        pass
-        
-
+        pass 
 
 if __name__ == "__main__":
-    window = Window()
-    app = App(window)
-    window.mainloop()
+   Window()
