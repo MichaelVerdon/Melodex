65b2a1ae4f6ae9ec64a4280e653f078746984ad4
Alvin-Fung <alvin_fung9@outlook.com>
2023-06-05 16:23:46 +0100

    Window class

--- gui.py
+++ gui.py
@@ -1,3 +1,17 @@
 import tkinter as tk
 from tkinter import *
 
+#class for making a window
+class Window(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("Melodex")
+        self.attributes= {
+            "bg": 'white',
+            "width":'1024',
+            "height": '768'  
+        }
+        self.maxsize = (1024,768)
+        self.minsize = (1024, 768)
+        self.resizable(0,0)
+        self.configure(self.attributes)
