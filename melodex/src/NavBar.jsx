11d42a61a3c2f10c8677d48e2623c224d7303aac
MichaelVerdon <mwverdon2000@gmail.com>
2023-08-29 10:15:39 +0100

    more stuff

--- melodex/src/NavBar.jsx
+++ melodex/src/NavBar.jsx
@@ -1,4 +1,12 @@
+import { useState } from 'react';
+
 const NavBar = () => {
+
+    const [signedIn, signIn] = useState(false);
+
+    const signUserIn = () => {
+        signIn(!signedIn);
+    }
     return(
         <>
             <nav className="nav">
@@ -7,6 +15,13 @@ const NavBar = () => {
                         <h1>Melodex</h1>
                     </a>
                 </div>
+                <ul className="searchList">
+                    <li>YouTube</li>
+                    <li>SoundCloud</li>
+                    {signedIn ? 
+                    <button onClick={signUserIn}>Sign In</button> :
+                    <button onClick={signUserIn}>Sign Out</button>}
+                </ul>
                 <div className="SearchContainer">
                     <input/>
                     <img src="../images/nav/search.svg" className="Search"/>
