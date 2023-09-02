d7d93b1cf1983258f92ea0dcae06149c0d3eab9a
MichaelVerdon <mwverdon2000@gmail.com>
2023-09-02 17:48:50 +0100

    stuff

--- server/search-utils/YouTube.py
+++ server/search-utils/YouTube.py
@@ -1,20 +1,28 @@
-import YTKey
+import sys
+sys.path.append("../API-Keys")
 import googleapiclient.discovery
 
 class YouTube:
 
     def __init__(self, query):
         self.query = query
-        self.yt = googleapiclient.discovery.build('youtube', 'v3', developerKey=YTKey.YTDevKey)
+        self.yt = googleapiclient.discovery.build('youtube', 'v3', developerKey="AIzaSyDTrZ3ZJl-eitRmuPAhTlC_WehaFBM2BFM")
 
     def search(self):
 
-        response = yt.search().list(
+        # Call the search.list method to retrieve videos
+        search_response = yt.search().list(
             q=self.query,
-            type="video",
+            type='video',
             part='id,snippet',
-            maxResults=10
+            maxResults=2
         ).execute()
+
+        # Parse the response and print video details
+        for search_result in search_response.get('items', []):
+            video_id = search_result['id']['videoId']
+            video_title = search_result['snippet']['title']
+            print(f'Video Title: {video_title}\nVideo ID: {video_id}\n')
 
 
 
