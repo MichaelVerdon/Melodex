b7eb62b5460412a8785bed5782951103eb7ff9c2
MichaelVerdon <mwverdon2000@gmail.com>
2023-09-02 18:03:19 +0100

    it works lmao

--- server/search-utils/YouTube.py
+++ server/search-utils/YouTube.py
@@ -1,30 +1,34 @@
-import sys
-sys.path.append("../API-Keys")
-import googleapiclient.discovery
+import YTKey
+from googleapiclient.discovery import build
 
 class YouTube:
 
     def __init__(self, query):
         self.query = query
-        self.yt = googleapiclient.discovery.build('youtube', 'v3', developerKey="AIzaSyDTrZ3ZJl-eitRmuPAhTlC_WehaFBM2BFM")
+        self.yt = build('youtube', 'v3', developerKey=YTKey.YTDevKey)
 
     def search(self):
 
-        # Call the search.list method to retrieve videos
-        search_response = yt.search().list(
+        response = self.yt.search().list(  # Use self.yt instead of yt
             q=self.query,
-            type='video',
             part='id,snippet',
-            maxResults=2
+            maxResults=10
         ).execute()
 
-        # Parse the response and print video details
-        for search_result in search_response.get('items', []):
-            video_id = search_result['id']['videoId']
-            video_title = search_result['snippet']['title']
-            print(f'Video Title: {video_title}\nVideo ID: {video_id}\n')
+        videos = []
+        channels = []
 
+        for search_result in response.get('items', []):
+            if search_result['id']['kind'] == 'youtube#video':
+                videos.append('%s (%s)' % (search_result['snippet']['title'],
+                                            search_result['id']['videoId']))
+            elif search_result['id']['kind'] == 'youtube#channel':
+                channels.append('%s (%s)' % (search_result['snippet']['title'],
+                                            search_result['id']['channelId']))
+                
+        print(videos, channels)
 
+        # Process the response as needed
 
 yt = YouTube(query="death grips hacker")
 yt.search()
