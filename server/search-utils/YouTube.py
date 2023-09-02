b9a8a77eec51d65ac4b385df7cf5d6fcce4a8e29
MichaelVerdon <mwverdon2000@gmail.com>
2023-09-02 16:04:17 +0100

    api stuff

--- server/search-utils/YouTube.py
+++ server/search-utils/YouTube.py
@@ -1,39 +1,22 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-import requests
+import YTKey
+import googleapiclient.discovery
 
 class YouTube:
 
     def __init__(self, query):
         self.query = query
-        self.base = "https://www.youtube.com/results?search_query=" # standard search link
-        self.filter ="&sp=EgIQAQ%253D%253D" # music filter
+        self.yt = googleapiclient.discovery.build('youtube', 'v3', developerKey=YTKey.YTDevKey)
 
     def search(self):
-        link = self.base + self.query.replace(" ", "+") + self.filter
-        # Ensure it does not open browser when scraping
-        options = webdriver.ChromeOptions()
-        options.add_argument("--headless")
-        driver = webdriver.Chrome(options=options)
 
-        try:
-            response = requests.get(link)
+        response = yt.search().list(
+            q=self.query,
+            type="video",
+            part='id,snippet',
+            maxResults=10
+        ).execute()
 
-            if response.status_code == 200:
-                driver.get(link)
-                results = driver.find_elements(By.XPATH, '//*[@id="dismissible"]')
 
-                
-                for result in results:
-                    title_element = result.find_element(By.XPATH, './/h3[@class="title-and-badge style-scope ytd-video-renderer"]')
-                    title = title_element.text.strip()
-                    print(title)
-
-                driver.quit()
-            else:
-                print("Site cannot be reached")
-        except Exception as e:
-            print("Error:", str(e))
 
 yt = YouTube(query="death grips hacker")
 yt.search()
